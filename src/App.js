a2bb05c77b4429dc0619a8d066a46d12a526d5b7
Travis <travis@travis.media>
2020-11-08 15:57:48 -0500

    adding shop page and components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 import "./App.css";
-import { Switch } from "react-router-dom/cjs/react-router-dom.min";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import { Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
